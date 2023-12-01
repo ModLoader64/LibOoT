d28538f8d477294bf748baac1d8f0e620385b592
Psi-Hate <dianaelzascudder@gmail.com>
2023-11-30 22:09:43 -0600

    Create Handlers.cs

---
+++ OoT/src/Handlers.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace OoT;
+
+public class Handlers
+{
+
+    [OnEmulatorStart]
+    public static void OnEmulatorStarted(EventEmulatorStart e)
+    {
+        Console.WriteLine("[OoT] Emulator Started.");
+    }
+
+}
